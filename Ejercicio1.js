3fa44686ebc745d89e7286366be656c9818e2b3d
Cpitoig <ignacioagus2010@gmail.com>
2025-09-18 15:11:26 -0300

    Add files via upload

---
+++ Ejercicio1.js
@@ -0,0 +1,10 @@
+alert("Hola mundo");
+
+let btn=document.getElementById("boton")
+
+let parrafo=document.getElementById("texto")
+
+btn.addEventListener("click",function(){
+    parrafo.innerHTML="salagonzalsabolognesa";
+    
+});
