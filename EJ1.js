aba6582152179f468f6b10a8d756c2f5557019b6
Cpitoig <ignacioagus2010@gmail.com>
2025-09-25 15:18:12 -0300

    Add files via upload

---
+++ EJ1.js
@@ -0,0 +1,9 @@
+function pedirNombre() {
+ let nombre = prompt("¿Cómo te llamas?");
+ alert("Hola " + nombre + ", ¡bienvenido a la clase de JavaScript!");
+}
+if (nombre) {
+ alert("Hola " + nombre + ", ¡bienvenido a la clase de JavaScript!");
+ } else {
+ alert("No escribiste tu nombre, inténtalo de nuevo.");
+ } 
